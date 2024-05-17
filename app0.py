95f2ddec6a00a8182d7bd4d32cb3c25b38e8ac3a
henri <augeh@yahoo.fr>
2024-05-17 11:29:24 +0200

    Add files via upload

---
+++ app0.py
@@ -0,0 +1,13 @@
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+from PIL import Image
+
+# ---- HEADER SECTION ----
+with st.container():
+    st.subheader("Hi, I am Riton :wave:")
+    st.title("A Data Analyst From France")
+    st.write(
+        "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings."
+    )
+    st.write("[Learn More >](https://yogalonsaccueil.webnode.fr/)")
